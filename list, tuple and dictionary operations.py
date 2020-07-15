8385d806cd210aeaa1b4b5ee64a3dac9d6837148
ishabansal3010 <68283712+ishabansal3010@users.noreply.github.com>
2020-07-15 16:02:16 +0530

    Basic operation on list, tuple and dictionary

---
+++ list, tuple and dictionary operations.py
@@ -0,0 +1,14 @@
+#Assigning elements to a list
+l1 = [19, 4, 78, 3, 56]
+l2 = ['a', 'b', 'c', 'd', 'e']
+
+#Accessing elements from a tuple
+t1 = (10, 9, 8, 7, 6)
+print (t1)
+print (t1[2])
+
+#deleting different dictionary elements
+d1 = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4}
+print(d1)
+del d1['b']
+print(d1)
